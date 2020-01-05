c16af5e8cf763a208c7be1f0d409e521230eae12
dschawel <david.schawel@gmail.com>
2020-01-04 18:35:48 -0800

    Created a route to create a new memory. Stubbed out route to post memory.

--- controllers/memory.js
+++ controllers/memory.js
@@ -5,8 +5,23 @@ router.get('/', (req, res) => {
     res.render('memories/main')
 })
 
+router.get('/new', (req, res) => {
+    res.render('memories/new')
+})
+
 router.get('/:id', (req, res) => {
     res.send('memory by id')
+})
+
+router.post('/', (req, res) => {
+    db.place.create({
+        name: req.body.name,
+        city: req.body.city,
+        state: req.body.state,
+        country: req.body.country,
+        description: req.body.description
+    })
+    res.redirect('/memories/main', { place })
 })
 
 module.exports = router 
