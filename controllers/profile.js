d800d1a6be48a6b7b062ce97ba5e02aefdd63dad
dschawel <david.schawel@gmail.com>
2020-01-07 10:38:15 -0800

    Able to now upload a profile image on profile page if profile isn't created with an image.

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,9 +1,27 @@
 let router = require('express').Router()
 let isAdminLoggedIn = require('../middleware/isAdminLoggedIn')
 let isLoggedIn = require('../middleware/isLoggedIn')
+let db = require('../models')
 
 router.get('/', isLoggedIn, (req, res) => {
     res.render('profile/main')
+})
+
+router.put('/', isLoggedIn, (req, res) => {
+    console.log(req.body)
+    db.user.update(
+        {photoUrl: req.body.photoUrl},
+        {where: { id: req.user.id }}
+    )
+    .then(() => {
+        console.log('SUCCESS')
+        res.send({'message': 'success'})
+    })
+    .catch(err => {
+        console.log('Error', err)
+        res.send({'message': 'error'})
+    })
+    
 })
 
 router.get('/admin', isAdminLoggedIn, (req, res) => {
--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.set('view engine', 'ejs')
 app.use(layouts)
 app.use('/', express.static('static'))
 app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
