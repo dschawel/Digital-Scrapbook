2de866ce7d4bb93f5c06e747c634763f51958310
dschawel <david.schawel@gmail.com>
2020-01-08 15:18:42 -0800

    Made the profile picture circular and made the memory image bigger.

--- static/app.js
+++ static/app.js
@@ -1,14 +1,14 @@
-<script>
-    let place = <%- place %>
-</script>
-<script>
-	mapboxgl.accessToken = 'pk.eyJ1IjoiZHNjaGF3ZWwiLCJhIjoiY2s0YWl3ankwMDRkaTNucnVqZGtvNWVrbCJ9._FgRj_tMA-T2lGsQq-nZRA';
-    var map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: 'mapbox://styles/mapbox/outdoors-v11', //stylesheet location
-    center: [11.255, 43.77], // starting position
-    zoom: 13 // starting zoom
-    });
+// {/* <script>
+//     let place = <%- place %>
+// </script>
+// <script>
+// 	mapboxgl.accessToken = 'pk.eyJ1IjoiZHNjaGF3ZWwiLCJhIjoiY2s0YWl3ankwMDRkaTNucnVqZGtvNWVrbCJ9._FgRj_tMA-T2lGsQq-nZRA';
+//     var map = new mapboxgl.Map({
+//     container: 'map', // container id
+//     style: 'mapbox://styles/mapbox/outdoors-v11', //stylesheet location
+//     center: [11.255, 43.77], // starting position
+//     zoom: 13 // starting zoom
+//     });
     
-    map.addControl(new mapboxgl.FullscreenControl());
-</script>
+//     map.addControl(new mapboxgl.FullscreenControl());
+// </script> */}
